5cc3f73fa7c5aeb082872f199ebf3152508ad1f7
ludwings <ludwings0330@gmail.com>
2022-02-06 20:53:18 +0900

    feat: 사용자 입력을 받는 기능 추가
    - 사용자가 입력한 값이 문자인지 숫자인지 확인
    - 사용자가 입력한 값이 숫자일때 올바른 숫자인지 확인

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,26 +1,86 @@
 package baseball;
 
+import nextstep.utils.Console;
 import nextstep.utils.Randoms;
 
 public class BaseBallGame {
     private final int RANGE_START = 123;
     private final int RANGE_END = 987;
-
+    private final String RESTART = "1";
     private int gameNumber;
 
+
+
     public void play() {
-        createGameNumber();
-        System.out.println("gameNumber is : " + gameNumber);
+        do {
+            int playerInputNumber = getPlayerInputNumber();
+            System.out.println("playerInputNumber = " + playerInputNumber);
+        } while (true);
+
     }
 
-    public void createGameNumber() {
+    private int getPlayerInputNumber() {
+        String playerInput = "";
+
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            playerInput = Console.readLine();
+        } while (!isValidPlayerInput(playerInput));
+
+        return Integer.parseInt(playerInput);
+    }
+
+    private boolean isValidPlayerInput(String playerInput) {
+        if (!isNumber(playerInput)) {
+            return false;
+        }
+
+        if (!isValidNumber(Integer.parseInt(playerInput))) {
+            System.out.println("ERROR : 올바른 숫자를 입력해주세요 ");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isNumber(String playerInput) {
+        try {
+            Integer.parseInt(playerInput);
+        } catch (Exception e) {
+            System.out.println("ERROR : 숫자를 입력해주세요");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void start() {
+
+        do {
+            setGameNumber(createGameNumber());
+            System.out.println("gameNumber is : " + gameNumber);
+
+            play();
+
+        } while (confirmRestart());
+    }
+
+    private boolean confirmRestart() {
+        System.out.println("게임르 새로 시작하려 1, 종료하려면 2를 입력하세요.");
+
+        String playerInput = Console.readLine();
+
+        return playerInput.equals(RESTART) ? true : false;
+    }
+
+    public int createGameNumber() {
         int pickedNumber;
 
         do {
             pickedNumber = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
         } while (!isValidNumber(pickedNumber));
 
-        setGameNumber(pickedNumber);
+        return pickedNumber;
     }
 
     public void setGameNumber(int pickedNumber) {
